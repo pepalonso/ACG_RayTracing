c5b3376fafa0c7b00259cc1d8282a2f32343b014
Evgeni Slavyanov Stavrev <evgenislavyanovstavrev@poblenou-130-194.eduroamcp.upf.edu>
2025-11-28 15:42:33 +0100

    feat: Implementing AO in NEE

--- src/shaders/nexteventestimatorintegration.h
+++ src/shaders/nexteventestimatorintegration.h
@@ -6,7 +6,7 @@
 class NextEventEstimatorIntegrator : public Shader
 {
 public:
-    NextEventEstimatorIntegrator(Vector3D bgColor_, int maxDepth_);
+    NextEventEstimatorIntegrator(Vector3D bgColor_, int maxDepth_, int aoSamples_ = 0, float aoMaxDistance_ = 0.3f);
 
     virtual Vector3D computeColor(const Ray &r,
                              const std::vector<Shape*> &objList,
@@ -14,6 +14,8 @@ public:
 
 private:
     int maxDepth;
+    int aoSamples;        // Number of AO samples (0 = disabled)
+    float aoMaxDistance;  // Maximum distance for AO occlusion testing
     
     Vector3D computeReflectedRadiance(const Vector3D& x,
                                      const Vector3D& n,
@@ -43,6 +45,10 @@ private:
     
     bool computeVisibility(const Vector3D& x, const Vector3D& y,
                           const std::vector<Shape*>& objList) const;
+    
+    float computeAmbientOcclusion(const Vector3D& x,
+                                 const Vector3D& n,
+                                 const std::vector<Shape*>& objList) const;
 };
 
 #endif // NEXTEVENTESTIMATORINTEGRATOR_H
